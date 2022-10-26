831790c9a3e6f59785846d9ac9ef4462ed43bc25
MrVtR <30987772+MrVtR@users.noreply.github.com>
2022-10-26 02:19:50 -0300

    Added initial finished version of font Dict Creator

---
+++ mcdFontMaker.py
@@ -0,0 +1,619 @@
+from pprint import pprint
+font = {
+  "fonts": [
+        {
+            "id": 0,
+            "symbols": []
+        },
+        {
+            "id": 1,
+            "symbols": [
+                {
+                    "char": "C",
+                    "glyph_id": 0
+                },
+                {
+                    "char": "L",
+                    "glyph_id": 1
+                },
+                {
+                    "char": "N",
+                    "glyph_id": 2
+                },
+                {
+                    "char": "P",
+                    "glyph_id": 3
+                },
+                {
+                    "char": "R",
+                    "glyph_id": 4
+                },
+                {
+                    "char": "S",
+                    "glyph_id": 5
+                },
+                {
+                    "char": "a",
+                    "glyph_id": 6
+                },
+                {
+                    "char": "b",
+                    "glyph_id": 7
+                },
+                {
+                    "char": "c",
+                    "glyph_id": 8
+                },
+                {
+                    "char": "d",
+                    "glyph_id": 9
+                },
+                {
+                    "char": "e",
+                    "glyph_id": 10
+                },
+                {
+                    "char": "f",
+                    "glyph_id": 11
+                },
+                {
+                    "char": "g",
+                    "glyph_id": 12
+                },
+                {
+                    "char": "i",
+                    "glyph_id": 13
+                },
+                {
+                    "char": "j",
+                    "glyph_id": 14
+                },
+                {
+                    "char": "l",
+                    "glyph_id": 15
+                },
+                {
+                    "char": "n",
+                    "glyph_id": 16
+                },
+                {
+                    "char": "o",
+                    "glyph_id": 17
+                },
+                {
+                    "char": "p",
+                    "glyph_id": 18
+                },
+                {
+                    "char": "q",
+                    "glyph_id": 19
+                },
+                {
+                    "char": "r",
+                    "glyph_id": 20
+                },
+                {
+                    "char": "s",
+                    "glyph_id": 21
+                },
+                {
+                    "char": "t",
+                    "glyph_id": 22
+                },
+                {
+                    "char": "u",
+                    "glyph_id": 23
+                },
+                {
+                    "char": "v",
+                    "glyph_id": 24
+                },
+                {
+                    "char": "y",
+                    "glyph_id": 25
+                },
+                {
+                    "char": "\u00f3",
+                    "glyph_id": 26
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "symbols": [
+                {
+                    "char": "A",
+                    "glyph_id": 27
+                },
+                {
+                    "char": "B",
+                    "glyph_id": 28
+                },
+                {
+                    "char": "C",
+                    "glyph_id": 29
+                },
+                {
+                    "char": "D",
+                    "glyph_id": 30
+                },
+                {
+                    "char": "E",
+                    "glyph_id": 31
+                },
+                {
+                    "char": "F",
+                    "glyph_id": 32
+                },
+                {
+                    "char": "I",
+                    "glyph_id": 33
+                },
+                {
+                    "char": "L",
+                    "glyph_id": 34
+                },
+                {
+                    "char": "M",
+                    "glyph_id": 35
+                },
+                {
+                    "char": "N",
+                    "glyph_id": 36
+                },
+                {
+                    "char": "O",
+                    "glyph_id": 37
+                },
+                {
+                    "char": "P",
+                    "glyph_id": 38
+                },
+                {
+                    "char": "R",
+                    "glyph_id": 39
+                },
+                {
+                    "char": "S",
+                    "glyph_id": 40
+                },
+                {
+                    "char": "T",
+                    "glyph_id": 41
+                },
+                {
+                    "char": "U",
+                    "glyph_id": 42
+                },
+                {
+                    "char": "\u00da",
+                    "glyph_id": 43
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "symbols": [
+                {
+                    "char": ",",
+                    "glyph_id": 44
+                },
+                {
+                    "char": ".",
+                    "glyph_id": 45
+                },
+                {
+                    "char": "0",
+                    "glyph_id": 46
+                },
+                {
+                    "char": "1",
+                    "glyph_id": 47
+                },
+                {
+                    "char": "2",
+                    "glyph_id": 48
+                },
+                {
+                    "char": "7",
+                    "glyph_id": 49
+                },
+                {
+                    "char": "A",
+                    "glyph_id": 50
+                },
+                {
+                    "char": "C",
+                    "glyph_id": 51
+                },
+                {
+                    "char": "D",
+                    "glyph_id": 52
+                },
+                {
+                    "char": "E",
+                    "glyph_id": 53
+                },
+                {
+                    "char": "G",
+                    "glyph_id": 54
+                },
+                {
+                    "char": "I",
+                    "glyph_id": 55
+                },
+                {
+                    "char": "L",
+                    "glyph_id": 56
+                },
+                {
+                    "char": "N",
+                    "glyph_id": 57
+                },
+                {
+                    "char": "O",
+                    "glyph_id": 58
+                },
+                {
+                    "char": "P",
+                    "glyph_id": 59
+                },
+                {
+                    "char": "Q",
+                    "glyph_id": 60
+                },
+                {
+                    "char": "R",
+                    "glyph_id": 61
+                },
+                {
+                    "char": "S",
+                    "glyph_id": 62
+                },
+                {
+                    "char": "T",
+                    "glyph_id": 63
+                },
+                {
+                    "char": "U",
+                    "glyph_id": 64
+                },
+                {
+                    "char": "X",
+                    "glyph_id": 65
+                },
+                {
+                    "char": "a",
+                    "glyph_id": 66
+                },
+                {
+                    "char": "b",
+                    "glyph_id": 67
+                },
+                {
+                    "char": "c",
+                    "glyph_id": 68
+                },
+                {
+                    "char": "d",
+                    "glyph_id": 69
+                },
+                {
+                    "char": "e",
+                    "glyph_id": 70
+                },
+                {
+                    "char": "g",
+                    "glyph_id": 71
+                },
+                {
+                    "char": "h",
+                    "glyph_id": 72
+                },
+                {
+                    "char": "i",
+                    "glyph_id": 73
+                },
+                {
+                    "char": "l",
+                    "glyph_id": 74
+                },
+                {
+                    "char": "m",
+                    "glyph_id": 75
+                },
+                {
+                    "char": "n",
+                    "glyph_id": 76
+                },
+                {
+                    "char": "o",
+                    "glyph_id": 77
+                },
+                {
+                    "char": "p",
+                    "glyph_id": 78
+                },
+                {
+                    "char": "r",
+                    "glyph_id": 79
+                },
+                {
+                    "char": "s",
+                    "glyph_id": 80
+                },
+                {
+                    "char": "t",
+                    "glyph_id": 81
+                },
+                {
+                    "char": "u",
+                    "glyph_id": 82
+                },
+                {
+                    "char": "v",
+                    "glyph_id": 83
+                },
+                {
+                    "char": "y",
+                    "glyph_id": 84
+                },
+                {
+                    "char": "\u00a9",
+                    "glyph_id": 85
+                }
+            ]
+        },
+        {
+            "id": 36,
+            "symbols": [
+                {
+                    "char": "\"",
+                    "glyph_id": 86
+                },
+                {
+                    "char": ".",
+                    "glyph_id": 87
+                },
+                {
+                    "char": "0",
+                    "glyph_id": 88
+                },
+                {
+                    "char": "1",
+                    "glyph_id": 89
+                },
+                {
+                    "char": "2",
+                    "glyph_id": 90
+                },
+                {
+                    "char": "3",
+                    "glyph_id": 91
+                },
+                {
+                    "char": "4",
+                    "glyph_id": 92
+                },
+                {
+                    "char": "6",
+                    "glyph_id": 93
+                },
+                {
+                    "char": "8",
+                    "glyph_id": 94
+                },
+                {
+                    "char": "9",
+                    "glyph_id": 95
+                },
+                {
+                    "char": ":",
+                    "glyph_id": 96
+                },
+                {
+                    "char": "A",
+                    "glyph_id": 97
+                },
+                {
+                    "char": "C",
+                    "glyph_id": 98
+                },
+                {
+                    "char": "E",
+                    "glyph_id": 99
+                },
+                {
+                    "char": "H",
+                    "glyph_id": 100
+                },
+                {
+                    "char": "I",
+                    "glyph_id": 101
+                },
+                {
+                    "char": "M",
+                    "glyph_id": 102
+                },
+                {
+                    "char": "N",
+                    "glyph_id": 103
+                },
+                {
+                    "char": "P",
+                    "glyph_id": 104
+                },
+                {
+                    "char": "R",
+                    "glyph_id": 105
+                },
+                {
+                    "char": "S",
+                    "glyph_id": 106
+                },
+                {
+                    "char": "U",
+                    "glyph_id": 107
+                },
+                {
+                    "char": "Y",
+                    "glyph_id": 108
+                },
+                {
+                    "char": "a",
+                    "glyph_id": 109
+                },
+                {
+                    "char": "b",
+                    "glyph_id": 110
+                },
+                {
+                    "char": "c",
+                    "glyph_id": 111
+                },
+                {
+                    "char": "d",
+                    "glyph_id": 112
+                },
+                {
+                    "char": "e",
+                    "glyph_id": 113
+                },
+                {
+                    "char": "f",
+                    "glyph_id": 114
+                },
+                {
+                    "char": "g",
+                    "glyph_id": 115
+                },
+                {
+                    "char": "h",
+                    "glyph_id": 116
+                },
+                {
+                    "char": "i",
+                    "glyph_id": 117
+                },
+                {
+                    "char": "j",
+                    "glyph_id": 118
+                },
+                {
+                    "char": "l",
+                    "glyph_id": 119
+                },
+                {
+                    "char": "m",
+                    "glyph_id": 120
+                },
+                {
+                    "char": "n",
+                    "glyph_id": 121
+                },
+                {
+                    "char": "o",
+                    "glyph_id": 122
+                },
+                {
+                    "char": "p",
+                    "glyph_id": 123
+                },
+                {
+                    "char": "r",
+                    "glyph_id": 124
+                },
+                {
+                    "char": "s",
+                    "glyph_id": 125
+                },
+                {
+                    "char": "t",
+                    "glyph_id": 126
+                },
+                {
+                    "char": "u",
+                    "glyph_id": 127
+                },
+                {
+                    "char": "y",
+                    "glyph_id": 128
+                },
+                {
+                    "char": "z",
+                    "glyph_id": 129
+                },
+                {
+                    "char": "\u00e1",
+                    "glyph_id": 130
+                },
+                {
+                    "char": "\u00e9",
+                    "glyph_id": 131
+                },
+                {
+                    "char": "\u00ed",
+                    "glyph_id": 132
+                },
+                {
+                    "char": "\u00f3",
+                    "glyph_id": 133
+                },
+                {
+                    "char": "\u00fa",
+                    "glyph_id": 134
+                },
+                {
+                    "char": "\uff1a",
+                    "glyph_id": 135
+                }
+            ]
+        }
+    ]
+}
+
+cont=0
+
+bytemessage = bytearray()
+
+for fontEntry in font['fonts']:
+    print("ID:",fontEntry['id'])
+    # ID da fonte - Uma vez por fontEntry
+    idFonte =fontEntry['id'] #int
+    idFonteByte = idFonte.to_bytes(2, 'little')
+
+    for charEntry in fontEntry['symbols']:
+        print("Escrevendo:",charEntry['char'],end='\n')
+        # Bytes do ID da fonte
+        for byte in idFonteByte:
+            bytemessage.append(byte)
+
+        # Char da fonte
+        charInt = ord(charEntry['char'])
+        charByte = charInt.to_bytes(2, 'little')
+        for byte in charByte:
+            bytemessage.append(byte)
+
+        # ID do char da fonte
+        idCharInt = charEntry['glyph_id']
+        idCharByte = idCharInt.to_bytes(2, 'little')
+        for byte in idCharByte:
+            bytemessage.append(byte)
+
+        # Bytes de término de string
+        for _ in range(2):
+            bytemessage.append(0)
+
+    print()
+
+with open("font.mcd", "wb") as binary_file:
+    binary_file.write(bytemessage)
+        
+
+#Cadeia de escrita
+# idFonte + 00 + format(ord("ó"), "x") + 00 + idChar + 00 00 00
+# EX: 
+# 01 00 4C 00 01 00 00 00
+# 01 00 4C 00 01 00 00 00
+
+
